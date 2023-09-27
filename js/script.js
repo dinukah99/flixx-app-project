2b15ecc901d24e843872623e7bba2d72136b1cb4
dinukah <dinukahamarasinghe@gmail.com>
2023-09-28 01:26:00 +0530

    display popular movies

--- js/script.js
+++ js/script.js
@@ -2,11 +2,41 @@ const global = {
     currentPage: window.location.pathname,
 };
 
-// Hightlight active link
+async function displayPopularMovies() {
+    const {results} = await fetchAPIData('movie/popular');
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+               ${movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+                class="card-img-top" alt="${movie.title}"/>` : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}"/>`}
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">
+                    <small class="text-muted">Release: ${movie.release_date}</small>
+                </p>
+            </div>`;
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+//Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+    const API_KEY = 'd6fabae4fc33b36f028ac6d196b5dbab';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language-en-US`);
+    const data = await response.json();
+    return data;
+}
+
+// Highlight active link
 function highlightActiveLink() {
     const links = document.querySelectorAll('.nav-link');
     links.forEach((link) => {
-        if (link.getAttribute('href') === global.currentPage) {
+        if ('/12-flixx-app-project/' + link.getAttribute('href') === global.currentPage) {
             link.classList.add('active');
         }
     });
@@ -16,19 +46,19 @@ function highlightActiveLink() {
 function init() {
     switch (global.currentPage) {
         case '/':
-        case '/index.html':
-            console.log('Home');
+        case '/12-flixx-app-project/index.html':
+            displayPopularMovies();
             break;
-        case '/shows.html':
+        case '/12-flixx-app-project/shows.html':
             console.log('Shows');
             break;
-        case '/movie-details.html':
-            console.log('Shows');
+        case '/12-flixx-app-project/movie-details.html':
+            console.log('Movie Details');
             break;
-        case '/tv-details.html':
-            console.log('TV');
+        case '/12-flixx-app-project/tv-details.html':
+            console.log('TV Details');
             break;
-        case '/search.html':
+        case '/12-flixx-app-project/search.html':
             console.log('Search');
             break;
     }
