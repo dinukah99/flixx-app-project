f617c25d54e5fa5ca51e53fda649e98b980f564e
dinukah <dinukahamarasinghe@gmail.com>
2023-09-28 01:37:39 +0530

    display popular tv shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
     currentPage: window.location.pathname,
 };
 
+//Display 20 most popular movies
 async function displayPopularMovies() {
     const {results} = await fetchAPIData('movie/popular');
     results.forEach((movie) => {
@@ -22,14 +23,45 @@ async function displayPopularMovies() {
     });
 }
 
+//Display 20 most popular tv shows
+async function displayPopularShows() {
+    const {results} = await fetchAPIData('tv/popular');
+    results.forEach((show) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${show.id}">
+               ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
+                class="card-img-top" alt="${show.name}"/>` : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}"/>`}
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${show.name}</h5>
+                <p class="card-text">
+                    <small class="text-muted">Air Date: ${show.first_air_date}</small>
+                </p>
+            </div>`;
+        document.querySelector('#popular-shows').appendChild(div);
+    });
+}
+
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = 'd6fabae4fc33b36f028ac6d196b5dbab';
     const API_URL = 'https://api.themoviedb.org/3/';
 
+    showSpinner();
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language-en-US`);
     const data = await response.json();
+    hideSpinner();
     return data;
+}
+
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -50,7 +82,7 @@ function init() {
             displayPopularMovies();
             break;
         case '/12-flixx-app-project/shows.html':
-            console.log('Shows');
+            displayPopularShows();
             break;
         case '/12-flixx-app-project/movie-details.html':
             console.log('Movie Details');
