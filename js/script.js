d4269430f8a3ef85e51c886e7717c4ba778d01a0
dinukah <dinukahamarasinghe@gmail.com>
2023-09-30 19:02:51 +0530

    code changes

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ async function displayPopularShows() {
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
-            <a href="movie-details.html?id=${show.id}">
+            <a href="tv-details.html?id=${show.id}">
                ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
                 class="card-img-top" alt="${show.name}"/>` : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}"/>`}
             </a>
@@ -50,6 +50,10 @@ async function displayPopularShows() {
 async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1];
     const movie = await fetchAPIData(`movie/${movieId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
     div.innerHTML = `
             <div class="details-top">
@@ -88,6 +92,76 @@ async function displayMovieDetails() {
             </div>`;
 
     document.querySelector('#movie-details').appendChild(div);
+}
+
+
+//Display Show Details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+            <div class="details-top">
+            <div>
+                ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
+                class="card-img-top" alt="${show.name}"/>` : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}"/>`}
+            </div>
+            <div>
+                <h2>${show.name}</h2>
+                <p>
+                    <i class="fas fa-star text-primary"></i>
+                    ${show.vote_average.toFixed(1)} / 10
+                </p>
+                <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+                <p>
+                    ${show.overview}
+                </p>
+                <h5>Genres</h5>
+                <ul class="list-group">
+                 ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+                </ul>
+                <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+            </div>
+        </div>
+        <div class="details-bottom">
+            <h2>Show Info</h2>
+            <ul>
+                <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+                <li><span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air}</li>
+                <li><span class="text-secondary">Status:</span> ${show.status}</li>
+            </ul>
+            <h4>Production Companies</h4>
+            <div class="list-group">
+                ${show.production_companies.map((company) => `<span> ${company.name}<br></span>`).join('')}
+            </div>`;
+
+    document.querySelector('#show-details').appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.2';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
 }
 
 //Fetch data from TMDB API
@@ -145,6 +219,7 @@ function init() {
         case '/tv-details':
         case '/tv-details.html':
         case '/12-flixx-app-project/tv-details.html':
+            displayShowDetails();
             break;
         case '/search':
         case '/search.html':
