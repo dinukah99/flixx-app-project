6a4273937e50fb746383e6737664325aeaf9f2a7
dinukah <dinukahamarasinghe@gmail.com>
2023-10-01 01:59:34 +0530

    image issue fixing

--- js/script.js
+++ js/script.js
@@ -269,7 +269,7 @@ async function displaySlider() {
         div.classList.add('swiper-slide');
         div.innerHTML = `
             <a href="movie-details.html?id=${movie.id}">
-                <img src="https:image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}"/>
+                <img src="https:image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}"/>
             </a>
             <h4 class="swiper-rating">
                 <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)}  / 10
